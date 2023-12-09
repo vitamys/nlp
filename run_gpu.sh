cf3efc4691f3157bc96787ba5a40935a6089c895
soldiervinz <vinzenz.uhr@gmail.com>
2023-12-09 14:40:32 +0100

    create python files for ubelix

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # You must specify a valid email address!
-#SBATCH --mail-user=
+#SBATCH --mail-user=vinzenz.uhr@gmail.com
 
 # Mail on NONE, BEGIN, END, FAIL, REQUEUE, ALL
-####SBATCH --mail-type=BEGIN,FAIL,END
+#SBATCH --mail-type=BEGIN,FAIL,END
 
 
 
@@ -33,13 +33,16 @@
 # module load CUDA/10.1.243
 # module load cuDNN/7.6.0.64-gcccuda-2019a
 
+singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install tensorboard
+singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install -U scikit-learn
+singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install datasets
 singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install transformers
 singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install matplotlib
 singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install pandas
 singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install spacy
 singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install nltk
-singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install ftfy regex tqdm
+singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install ftfy regex tqdm 
 #singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime pip install simpletransformers
 
 #### Start training,testing, evaluation ####
-singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime python3 project.py
+singularity exec --nv docker://pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime python3 project_split_rows_with_pp.py
